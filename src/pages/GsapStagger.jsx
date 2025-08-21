5e5ab544fec2e9a6fe9bde1888ef7889545fffa8
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-08-21 13:02:11 +0530

    Gsap-Stagger

--- src/pages/GsapStagger.jsx
+++ src/pages/GsapStagger.jsx
@@ -1,5 +1,26 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapStagger = () => {
   // TODO: Implement the gsap.stagger() method
+  useGSAP(() => {
+    gsap.to(".stagger-box", {
+      y: 250,
+      rotationY: 360,
+      borderRadius: "100%",
+      repeat: -1,
+      yoyo: true,
+      duration: 3,
+      // stagger: 0.5,
+      stagger: {
+        amount: 1.5,
+        grid: [3, 3],
+        from: "center",
+        axis: "y",
+        ease: "circ.inOut",
+      },
+    });
+  }, []);
 
   return (
     <main>
@@ -23,8 +44,7 @@ const GsapStagger = () => {
         <a
           href="https://gsap.com/resources/getting-started/Staggers"
           target="_blank"
-          rel="noreferrer noopener nofollow"
-        >
+          rel="noreferrer noopener nofollow">
           Gsap Stagger
         </a>{" "}
         feature.
@@ -37,6 +57,8 @@ const GsapStagger = () => {
           <div className="w-20 h-20 bg-indigo-400 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-500 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-600 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-700 rounded-lg stagger-box" />
+          <div className="w-20 h-20 bg-indigo-800 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-700 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-800 rounded-lg stagger-box" />
         </div>
